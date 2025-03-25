4679cd80dfb4a7b55d8d342c0c316dcc819382c0
edlin <edlin@tencent.com>
2025-03-25 12:46:32 +0800

    0325

--- 手写代码/note-手写leetcode.js
+++ 手写代码/note-手写leetcode.js
@@ -1,156 +1,217 @@
-// 一. 简单排序
-// 1.1. 冒泡排序
+// 1. 实现斐波那契数列
 
-// 1.2. 选择排序: 遍历平方,每次遍历找到最小的值放在最前面
+function fibo(n) {
+    if (n == 1 || n ==2) return n;
+    return fibo(n - 1) + fibo(n - 2)
+};
 
-// 1.3. 插入排序 : 遍历，从[1]开始，跟前面的元素进行比较，如果比它小，就位置往前挪 , for + while
 
-// 二. 重要排序
-// 2.1. 归并排序 : 递归，二分之一开始递归，分成了很多小的两个区间，将两个区间进行每个比较排好序
-
-// 2.2. 快速排序 : （递归）选中基准，然后分割左右，然后再将元素与基准进行比较，左边元素小于基准，右边元素大于基准
-
-// 2.3. 堆排序
-
-// 2.4. 希尔排序
-
-// 作者：coderwhy
-// 链接：https://juejin.cn/post/7208466455879467045
-
-// 1. 选择排序，遍历平方,每次遍历找到最小的值放在最前面,参数arr， 找到最小的值的下标，然后交换位置
-function selectionSort(arr: number[]): number[] {
-    for (let i = 0; i < arr.length; i++) {
-        let minIndex = i;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
-
-        if (i !== minIndex) {
-            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-        }
+var fibonacci = function (n) {
+    let n1 = 1, n2 = 1;
+    for (let i = 2; i <= n; i++) {
+        [n1, n2] = [n2, n1 + n2];
     }
-    return arr;
+    return n2;
 }
 
-// 2. 插入排序，遍历，从[1]开始，跟前面的元素进行比较，如果比它小，就位置往前挪，参数是arr , for + while
-function insertionSort(arr: number[]): number[] {
-    for (let i = 1; i < arr.length; i++) {
-        let current = arr[i];
-        let j = i - 1;
-        // 如果该元素小于前一个元素，那么前一个元素向后移动，并继续向前比较
-        while (j >= 0 && arr[j] > current) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-        // 如果该元素大于前一个元素，那么它将放到合适的位置
-        arr[j + 1] = current;
+fibonacci(2);
+
+// 2. 找到唯一的数
+
+function solution(cards) {
+    // Edit your code here
+    let result = 0;
+    for (let i = 0; i < cards.length; i++) {
+        result ^= cards[i];  // 使用异或运算
     }
-    return arr;
-}
-
-// 3. 归并排序,（Merge Sort）
-// 递归，二分之一开始递归，分成了很多小的两个区间，将两个区间进行每个比较排好序
-// 数组做参数，分割数组slice
-function mergeSort(arr: number[]): number[] {
-    const n = arr.length;
-    if (n <= 1) return arr;
-    const middle = Math.floor(n / 2);
-    const left = mergeSort(arr.slice(0, middle));
-    const right = mergeSort(arr.slice(middle));
-    return merge(left, right)
-}
-
-function merge(left: number[], right: number[]):number[] {
-    let i = 0, j = 0;
-    const result = [];
-    while (i < left.length && j < right.length) {
-        if (left[i] < right[j]) {
-            result.push(left[i++])
-        } else {
-            result.push(right[j++])
-        }
-    }
-
-    result.concat(left.slice(i)).concat(right.slice(j));
-
     return result;
 }
 
-// 4. 快递排序，
-// （递归）选中基准，然后分割左右，然后再将元素与基准进行比较，左边元素小于基准，右边元素大于基准
-// 参数，arr,low,high
-function quickSort(arr, low = 0, high = arr.length - 1) {
-    if (low < high) {
-        const pivotIndex = partition(arr, low, high);
-        quickSort(arr, low, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, high);
-    }
-    return arr;
-}
-function partition(arr, low, high) {
-    // 选择基准元素（此处选最后一个元素，实际可优化为随机选择）
-    const pivot = arr[high];
-    let i = low; // i 指向“小元素区”的末尾
+console.log(solution([1, 1, 2, 2, 3, 3, 4, 5, 5]) === 4);
+console.log(solution([0, 1, 0, 1, 2]) === 2);
 
-    // 遍历区间 [low, high-1]，将小元素交换到左侧
-    for (let j = low; j < high; j++) {
-        if (arr[j] < pivot) {
-            [arr[i], arr[j]] = [arr[j], arr[i]]; // 交换元素
-            i++; // 扩展“小元素区”
+// 3. 最小的花费
+function solution(n, k, data) {
+    let sum = 0;
+    for (let i = 0; i < data.length; i++) {
+        // 初始化当前最便宜的价格为当天的价格
+        let minPrice = data[i];
+        
+        // 检查前 k 天的价格，找到最便宜的那一天
+        for (let j = 1; j <= k && i - j >= 0; j++) {
+            if (data[i - j] < minPrice) {
+                minPrice = data[i - j];
+            }
+        }
+        
+        // 更新总花费
+        sum += minPrice;
+    }
+    return sum;
+}
+
+console.log(solution(5, 2, [1, 2, 3, 3, 2]) === 9);
+
+
+// 4. 数字格式化
+function solution(s) {
+    s = s.replace(/^0+/, '')
+    let index = s.indexOf('.');
+    if (index === -1) {
+        index = s.length;
+    }
+
+    let integerPart = '';
+    for (let i = index - 1, count = 0; i >= 0; i--, count++) {
+        if (count > 0 && count % 3 === 0) {
+            integerPart = ',' + integerPart;
+        }
+        integerPart = s[i] + integerPart;
+        console.log(i, integerPart);
+    }
+
+    let result = integerPart;
+    if (index != s.length) {
+        result += s.slice(index)
+    }
+    return result;
+}
+
+function main() {
+    console.log(solution("1294512.12412") === '1,294,512.12412');
+    console.log(solution("0000123456789.99") === '123,456,789.99');
+    console.log(solution("987654321") === '987,654,321');
+}
+
+// 5. 组合数字之和
+function solution(numbers) {
+
+    let length = numbers.length;
+    let sum = 0;
+    let strNum = numbers.map((item) => item.toString());
+    function generateCombinations(index, currentCombination) {
+        if (index === length) {
+            let currentSum = currentCombination.reduce((acc, digit) => acc + parseInt(digit), 0);
+
+            if (currentSum % 2 === 0) {
+                sum++;
+            }
+            return;
+        }
+        for (let digit of strNum) {
+            generateCombinations(index + 1, currentCombination.concat(digit))
         }
     }
 
-    // 将基准元素放到正确位置（i 右侧是大元素区）
-    [arr[i], arr[high]] = [arr[high], arr[i]];
-    return i; // 返回基准元素的最终位置
+    generateCombinations(0, []);
+
 }
 
+console.log(solution([123, 456, 789]) === 14);
 
 
-//  5. 二分查找 （手写） while + for , 参数arr, val ,
-//   定义low, high, 找到中间值mid, 然后比较mid和val, 如果mid大于val, 那么high = mid - 1, 
-//  如果mid小于val, 那么low = mid + 1, 如果mid等于val, 那么返回mid
-function binary(arr, val) {
-    if (!arr.length) return -1;
-    let low = 0, high = arr.length;
-    while (low < high) {
-        let mid = Math.floor((high + low) / 2);
-        for (let i = 0; i < arr.length; i++) {
-            if (arr[i] == val) {
-                return i;
-            }
-            else if (arr[i] > val) {
-                high = mid - 1;
-            } else if (arr[i] < val) {
-                low = mid + 1;
+// 6. 最大的葫芦
+function solution(n, max, array) {
+    // Edit your code here
+    // 牌面值的映射
+    let countMap = {};
+    array.forEach((item) => {
+        countMap[item] = (countMap[item] || 0) + 1;
+    })
+
+    let twoArr = [];
+    let threeArr = [];
+    for (let key in countMap) {
+        if (countMap[key] >= 3) {
+            threeArr.push(parseInt(key))
+        }
+        if (countMap[key] >= 2) {
+            twoArr.push(parseInt(key));
+        }
+    }
+
+    let possibleFours = [];
+    for (let three of threeArr) {
+        for (let two of twoArr) {
+            if (three !== two) {
+                possibleFours.push([three, two])
             }
         }
     }
-    return low
-}
 
-// 6. 二分插入，遍历，从[1]开始，跟前面的元素比较，只是每次从二分之一开始。 
-    // for + while 开始找最小的下标low，for 开始腾挪 ， 然受arr[low]设置
-function binaryInsertionSort(arr) {
-    for (let i = 1; i < arr.length; i++) {
-        const key = arr[i];
-        let low = 0, high = i - 1;
-        while (low <= high) {
-            const mid = Math.floor((low + high) / 2);
-            if (key < arr[mid]) {
-                high = mid - 1;
+    let vaildFours = possibleFours.filter(four => {
+        let sum = four[0] * 3 + four[1] * 2;
+        return sum <= max
+    })
+
+    // 葫芦
+    if (vaildFours.length === 0) {
+        return [0, 0];
+    } else {
+        vaildFours.sort((a, b) => {
+            if (a[0] !== b[0]) {
+                return b[0] - a[0];
             } else {
-                low = mid + 1;
+                return b[1] - a[1];
             }
-        }
-        // 移动
-        for (let j = i; j > low; j--) {
-            arr[j] = arr[j - 1];
-        }
-
-        arr[low] = key;
+        })
+        // return vaildFours[0];
+        // 处理1的情况， tips: find找不到就返回undefined
+        let temp1Fours = vaildFours.find((item) => item[0] == 1);
+        let maxFours = temp1Fours === undefined ? vaildFours[0] : temp1Fours;
+        let temp2Fours = vaildFours.find((item) => item[0] === maxFours[0] && item[1] === 1);
+        maxFours = temp2Fours === undefined ? maxFours : temp2Fours
+        return maxFours;
     }
-    return arr;
 }
+
+// 统计每种牌的数量：使用 countMap 来记录每种牌面值的数量。
+// 找出所有可能的三张相同牌和两张相同牌的组合：遍历 countMap，找出数量大于等于3的牌面值（三张相同牌）和数量大于等于2的牌面值（两张相同牌）。
+// 生成所有可能的葫芦组合：遍历 threeArr 和 twoArr，生成所有可能的葫芦组合。
+// 筛选符合条件的葫芦：计算每个葫芦的牌面值之和，并筛选出符合条件的葫芦。
+// 选择最大的葫芦：在符合条件的葫芦中，选择牌面值最大的葫芦, (最后记得处理1的情况)
+
+
+
+//  7. DNA序列： https://blog.csdn.net/helan_ts/article/details/145676228?utm_medium=distribute.pc_relevant.none-task-blog-2~default~baidujs_baidulandingword~default-0-145676228-blog-143196104.235^v43^pc_blog_bottom_relevance_base8&spm=1001.2101.3001.4242.1&utm_relevant_index=3
+function solution(dna1, dna2) {
+    const m = dna1.length;
+    const n = dna2.length;
+    
+    // 创建一个二维数组来存储编辑距离
+    const dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+    
+    Array.from({ length: m + 1}, () => Array(n + 1).fill(0));
+    // 初始化边界条件
+    for (let i = 0; i <= m; i++) {
+      dp[i][0] = i;
+    }
+    for (let j = 0; j <= n; j++) {
+      dp[0][j] = j;
+    }
+    
+    // 填充dp数组
+    for (let i = 1; i <= m; i++) {
+      for (let j = 1; j <= n; j++) {
+        if (dna1[i - 1] === dna2[j - 1]) {
+          dp[i][j] = dp[i - 1][j - 1];
+        } else {
+          dp[i][j] = Math.min(
+            dp[i - 1][j] + 1, // 删除
+            dp[i][j - 1] + 1, // 插入
+            dp[i - 1][j - 1] + 1 // 替换
+          );
+        }
+      }
+    }
+    
+    return dp[m][n];
+  }
+  
+  function main() {
+    // 你可以添加更多测试用例
+    console.log(solution("AGCTTAGC", "AGCTAGCT") === 2);
+    console.log(solution("AGCCGAGC", "GCTAGCT") === 4);
+  }
+  
+  main();
---
+++ 手写代码/note-手写算法.js
@@ -0,0 +1,156 @@
+// 一. 简单排序
+// 1.1. 冒泡排序
+
+// 1.2. 选择排序: 遍历平方,每次遍历找到最小的值放在最前面
+
+// 1.3. 插入排序 : 遍历，从[1]开始，跟前面的元素进行比较，如果比它小，就位置往前挪 , for + while
+
+// 二. 重要排序
+// 2.1. 归并排序 : 递归，二分之一开始递归，分成了很多小的两个区间，将两个区间进行每个比较排好序
+
+// 2.2. 快速排序 : （递归）选中基准，然后分割左右，然后再将元素与基准进行比较，左边元素小于基准，右边元素大于基准
+
+// 2.3. 堆排序
+
+// 2.4. 希尔排序
+
+// 作者：coderwhy
+// 链接：https://juejin.cn/post/7208466455879467045
+
+// 1. 选择排序，遍历平方,每次遍历找到最小的值放在最前面,参数arr， 找到最小的值的下标，然后交换位置
+function selectionSort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        if (i !== minIndex) {
+            [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+        }
+    }
+    return arr;
+}
+
+// 2. 插入排序，遍历，从[1]开始，跟前面的元素进行比较，如果比它小，就位置往前挪，参数是arr , for + while
+function insertionSort(arr: number[]): number[] {
+    for (let i = 1; i < arr.length; i++) {
+        let current = arr[i];
+        let j = i - 1;
+        // 如果该元素小于前一个元素，那么前一个元素向后移动，并继续向前比较
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        // 如果该元素大于前一个元素，那么它将放到合适的位置
+        arr[j + 1] = current;
+    }
+    return arr;
+}
+
+// 3. 归并排序,（Merge Sort）
+// 递归，二分之一开始递归，分成了很多小的两个区间，将两个区间进行每个比较排好序
+// 数组做参数，分割数组slice
+function mergeSort(arr: number[]): number[] {
+    const n = arr.length;
+    if (n <= 1) return arr;
+    const middle = Math.floor(n / 2);
+    const left = mergeSort(arr.slice(0, middle));
+    const right = mergeSort(arr.slice(middle));
+    return merge(left, right)
+}
+
+function merge(left: number[], right: number[]):number[] {
+    let i = 0, j = 0;
+    const result = [];
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            result.push(left[i++])
+        } else {
+            result.push(right[j++])
+        }
+    }
+
+    result.concat(left.slice(i)).concat(right.slice(j));
+
+    return result;
+}
+
+// 4. 快递排序，
+// （递归）选中基准，然后分割左右，然后再将元素与基准进行比较，左边元素小于基准，右边元素大于基准
+// 参数，arr,low,high
+function quickSort(arr, low = 0, high = arr.length - 1) {
+    if (low < high) {
+        const pivotIndex = partition(arr, low, high);
+        quickSort(arr, low, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, high);
+    }
+    return arr;
+}
+function partition(arr, low, high) {
+    // 选择基准元素（此处选最后一个元素，实际可优化为随机选择）
+    const pivot = arr[high];
+    let i = low; // i 指向“小元素区”的末尾
+
+    // 遍历区间 [low, high-1]，将小元素交换到左侧
+    for (let j = low; j < high; j++) {
+        if (arr[j] < pivot) {
+            [arr[i], arr[j]] = [arr[j], arr[i]]; // 交换元素
+            i++; // 扩展“小元素区”
+        }
+    }
+
+    // 将基准元素放到正确位置（i 右侧是大元素区）
+    [arr[i], arr[high]] = [arr[high], arr[i]];
+    return i; // 返回基准元素的最终位置
+}
+
+
+
+//  5. 二分查找 （手写） while + for , 参数arr, val ,
+//   定义low, high, 找到中间值mid, 然后比较mid和val, 如果mid大于val, 那么high = mid - 1, 
+//  如果mid小于val, 那么low = mid + 1, 如果mid等于val, 那么返回mid
+function binary(arr, val) {
+    if (!arr.length) return -1;
+    let low = 0, high = arr.length;
+    while (low < high) {
+        let mid = Math.floor((high + low) / 2);
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] == val) {
+                return i;
+            }
+            else if (arr[i] > val) {
+                high = mid - 1;
+            } else if (arr[i] < val) {
+                low = mid + 1;
+            }
+        }
+    }
+    return low
+}
+
+// 6. 二分插入，遍历，从[1]开始，跟前面的元素比较，只是每次从二分之一开始。 
+    // for + while 开始找最小的下标low，for 开始腾挪 ， 然受arr[low]设置
+function binaryInsertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        const key = arr[i];
+        let low = 0, high = i - 1;
+        while (low <= high) {
+            const mid = Math.floor((low + high) / 2);
+            if (key < arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        // 移动
+        for (let j = i; j > low; j--) {
+            arr[j] = arr[j - 1];
+        }
+
+        arr[low] = key;
+    }
+    return arr;
+}
