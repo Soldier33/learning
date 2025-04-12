d895df74012303a8d05478b5609c6d0bca5a26ea
edlin <edlin@tencent.com>
2025-04-12 11:01:09 +0800

    0412

--- 手写代码/note-手写代码实现.js
+++ 手写代码/note-手写代码实现.js
@@ -204,6 +204,32 @@ function all(promises) {
    }) 
   }
 
+
+  // Promise.allSettled()
+  function allSettled(promises) {
+    return new Promise((resolve, reject) => {
+      const results = [];
+      const len = promises.length;
+      let resolvedCount = 0;
+      for (let i = 0; i < len; i++) {
+        promises[i]
+          .then((value) => {
+            results[i] = { status: "fulfilled", value };
+          })
+          .catch((reason) => {
+            results[i] = { status: "rejected", reason };
+          })
+          .finally(() => {
+            resolvedCount++;
+            if (resolvedCount === len) {
+              resolve(results);
+            }
+          });
+      }
+    });
+  }
+  
+
   
 
   function any(promises) {
